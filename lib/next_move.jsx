f499bda5f08eb89258475745ca75574258932fab
Daniel <daniel.j.fletcher2@gmail.com>
2017-04-04 13:05:37 -0700

    Remove crime actions from window in entry file

--- lib/next_move.jsx
+++ lib/next_move.jsx
@@ -2,10 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
-import { requestAllCrimes, requestFilteredCrimes } from './actions/crimes_actions';
-
-window.requestAllCrimes = requestAllCrimes;
-window.requestFilteredCrimes = requestFilteredCrimes;
 
 document.addEventListener('DOMContentLoaded', () => {
   const store = configureStore();
