a29124f9a69d2a6f4824f991fc0b6475bae00bd5
Sweta <Swetagsanghavi@gmail.com>
2017-04-04 12:27:11 -0700

    Add react-heatmaplayer

--- lib/components/map/map.jsx
+++ lib/components/map/map.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
+import HeatmapLayer from 'react-leaflet-heatmap-layer';
+import { bartStops } from '../../../assets/data/bart_stops';
 
 class LeafletMap extends React.Component {
   constructor() {
@@ -14,9 +16,20 @@ class LeafletMap extends React.Component {
 
   render() {
     const position = [this.state.lat, this.state.lng];
+    const data = bartStops.map(el => ([el.stop_lat, el.stop_lon, 60]));
+    debugger;
     return (
       <div>
         <Map style={{height: "100vh"}} center={position} zoom={this.state.zoom}>
+          <HeatmapLayer
+            fitBoundsOnLoad
+            fitBoundsOnUpdate
+            points={data}
+            createLeafletElement={m => m}
+            longitudeExtractor={m =>
+              m[1]}
+            latitudeExtractor={m => m[0]}
+            intensityExtractor={m => parseFloat(m[2])} />
           <TileLayer
             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             url='https://api.mapbox.com/styles/v1/hellosweta/cj12k3v5n004l2rt89a28igfd/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiaGVsbG9zd2V0YSIsImEiOiJjajEyaDhwZnQwNnF5MzNvMms3dzluemZnIn0.RzmThYRkDkV3wEMw7J2JCA'
