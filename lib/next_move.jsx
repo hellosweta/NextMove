4e67febe399a833f82083be8db7648f8498b7314
Daniel <daniel.j.fletcher2@gmail.com>
2017-04-04 11:53:07 -0700

    Can filter crimes by category

--- lib/next_move.jsx
+++ lib/next_move.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { fetchAllCrimes, fetchSomeCrimes } from './util/crime_api_util';
 import configureStore from './store/store';
 import Root from './components/root';
+import { requestAllCrimes, requestFilteredCrimes } from './actions/crimes_actions';
+
+window.requestAllCrimes = requestAllCrimes;
+window.requestFilteredCrimes = requestFilteredCrimes;
 
 document.addEventListener('DOMContentLoaded', () => {
-  window.fetchAllCrimes = fetchAllCrimes;
-  window.fetchSomeCrimes = fetchSomeCrimes;
   const store = configureStore();
+  window.store = store;
   const root = document.getElementById('root');
   ReactDOM.render(<Root store={store}/>, root);
 });
