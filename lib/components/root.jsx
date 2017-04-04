b639c6db0c9a1e612d02f482b4524e4c51f3fe37
Sweta <Swetagsanghavi@gmail.com>
2017-04-04 10:08:47 -0700

    Add redux thunk, fix root_reducer errors

--- lib/components/root.jsx
+++ lib/components/root.jsx
@@ -3,10 +3,10 @@ import { Provider } from 'react-redux';
 import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 
 import App from './app';
-import LocationDetailContainer from './location_detail';
-import TransitDetailContainer from './transit_detail';
-import CrimeDetailContainer from './crime_detail';
-import RestaurantDetailContainer from './restaurant_detail';
+// import LocationDetailContainer from './location_detail';
+// import TransitDetailContainer from './transit_detail';
+// import CrimeDetailContainer from './crime_detail';
+// import RestaurantDetailContainer from './restaurant_detail';
 
 const Root = ({ store }) =>{
 
@@ -14,14 +14,6 @@ const Root = ({ store }) =>{
     <Provider store= { store }>
       <Router history= { hashHistory }>
         <Route path='/' component={ App }>
-          <Route path='search' component={ LocationDetailContainer }>
-            <Route path='search/crime'
-              component={ CrimeDetailContainer } />
-            <Route path='search/transit'
-               component={ TransitDetailContainer } />
-            <Route path='search/restaurant'
-              component={ RestaurantDetailContainer } />
-          </Route>
         </Route>
       </Router>
     </Provider>
@@ -29,3 +21,19 @@ const Root = ({ store }) =>{
 };
 
 export default Root;
+// return (
+//   <Provider store= { store }>
+//     <Router history= { hashHistory }>
+//       <Route path='/' component={ App }>
+//         <Route path='search' component={ LocationDetailContainer }>
+//           <Route path='search/crime'
+//             component={ CrimeDetailContainer } />
+//           <Route path='search/transit'
+//              component={ TransitDetailContainer } />
+//           <Route path='search/restaurant'
+//             component={ RestaurantDetailContainer } />
+//         </Route>
+//       </Route>
+//     </Router>
+//   </Provider>
+// );
