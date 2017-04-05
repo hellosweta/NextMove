c4e6c153ea8b7547aaf83d2701b4cfde0291e636
Daniel <daniel.j.fletcher2@gmail.com>
2017-04-05 11:14:32 -0700

    Change requestAllRestaurants to fetch about 5,000 restaurants

--- lib/next_move.jsx
+++ lib/next_move.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
+import { requestAllRestaurants } from './actions/restaurant_actions';
+
+window.requestAllRestaurants = requestAllRestaurants;
 
 document.addEventListener('DOMContentLoaded', () => {
   const store = configureStore();
