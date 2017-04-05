27565d333cdd40e0d055cdc6a5a1d04757f96c06
Daniel <daniel.j.fletcher2@gmail.com>
2017-04-05 11:38:59 -0700

    Remove requestAllCrimes from window

--- lib/next_move.jsx
+++ lib/next_move.jsx
@@ -2,9 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
-import { requestAllCrimes } from './actions/crimes_actions';
-
-window.requestAllCrimes = requestAllCrimes;
 
 document.addEventListener('DOMContentLoaded', () => {
   const store = configureStore();
