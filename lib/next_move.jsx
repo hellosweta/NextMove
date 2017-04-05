ebcb0e6e0fb26b9b7422789d8e9f42a99b27ce9a
Daniel <daniel.j.fletcher2@gmail.com>
2017-04-05 11:15:15 -0700

    Remove requestAllRestaurants from window

--- lib/next_move.jsx
+++ lib/next_move.jsx
@@ -2,9 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
-import { requestAllRestaurants } from './actions/restaurant_actions';
-
-window.requestAllRestaurants = requestAllRestaurants;
 
 document.addEventListener('DOMContentLoaded', () => {
   const store = configureStore();
