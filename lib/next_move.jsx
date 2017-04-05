c843a03b946bd3b0a9d62f80e0cfd9ea175fe59a
Daniel <daniel.j.fletcher2@gmail.com>
2017-04-05 11:38:29 -0700

    Complete more crimes from api fetch method

--- lib/next_move.jsx
+++ lib/next_move.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
+import { requestAllCrimes } from './actions/crimes_actions';
+
+window.requestAllCrimes = requestAllCrimes;
 
 document.addEventListener('DOMContentLoaded', () => {
   const store = configureStore();
