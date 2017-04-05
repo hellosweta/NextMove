894e0e8ac6198981fc506500c9bdc7a35217a9fa
Daniel <daniel.j.fletcher2@gmail.com>
2017-04-04 19:54:24 -0700

    Limited column types to receive in crime_api_util ajax requests

--- lib/next_move.jsx
+++ lib/next_move.jsx
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
+import { requestFilteredCrimes, requestAllCrimes } from './actions/crimes_actions';
+
 
 document.addEventListener('DOMContentLoaded', () => {
   const store = configureStore();
   window.store = store;
+  window.requestFilteredCrimes = requestFilteredCrimes;
+  window.requestAllCrimes = requestAllCrimes;
   const root = document.getElementById('root');
   ReactDOM.render(<Root store={store}/>, root);
 });
