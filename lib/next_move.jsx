96add9e8d7dea992140590f111030e4fc35fd86e
Daniel <daniel.j.fletcher2@gmail.com>
2017-04-04 20:25:01 -0700

    Rewrite restaurants api util to communicate with data.sfgov.org

--- lib/next_move.jsx
+++ lib/next_move.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
+import { requestAllRestaurants } from './actions/restaurant_actions';
 
 document.addEventListener('DOMContentLoaded', () => {
   const store = configureStore();
   window.store = store;
+  window.requestAllRestaurants = requestAllRestaurants;
   const root = document.getElementById('root');
   ReactDOM.render(<Root store={store}/>, root);
 });
