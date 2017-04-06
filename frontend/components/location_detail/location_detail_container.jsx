6bd8a4f2dc2054d02aed5169740ee849abc496b6
Sweta <Swetagsanghavi@gmail.com>
2017-04-06 11:04:58 -0700

    Add map intensity calculation, add scroll, add filteredCrime action to map

--- frontend/components/location_detail/location_detail_container.jsx
+++ frontend/components/location_detail/location_detail_container.jsx
@@ -4,20 +4,17 @@ import {requestFilteredCrimes} from '../../actions/crimes_actions';
 import LocationDetail from './location_detail';
 
 
-
 const mapStateToProps = state => ({
   filteredCrimes: state.filteredCrimes,
-  lat:        37.752242,
-  long:     -122.417433,
-  radius: .25
 });
 
 const mapDispatchToProps = dispatch => ({
-  requestFilteredCrimes:  (lat, lon, radiusInMiles) =>
-                dispatch(requestFilteredCrimes(lat, lon, radiusInMiles))
+
+  // requestFilteredCrimes:  (lat, lon, radiusInMiles) =>
+  //               dispatch(requestFilteredCrimes(lat, lon, radiusInMiles))
 });
 
 export default connect(
   mapStateToProps,
-  mapDispatchToProps
+  null
 )(LocationDetail);
--- frontend/components/map/map_container.js
+++ frontend/components/map/map_container.js
@@ -1,16 +1,20 @@
 import { connect } from 'react-redux';
 import { requestAllRestaurants } from '../../actions/restaurants_actions';
-import { requestAllCrimes } from '../../actions/crimes_actions';
+import { requestAllCrimes, requestFilteredCrimes } from '../../actions/crimes_actions';
+import { requestAllTransitData } from '../../actions/transit_actions';
 import Map from './map';
 
 const mapStateToProps = state =>({
-    allRestaurants: state.allRestaurants,
-    allCrimes: state.allCrimes
+  allRestaurants: state.allRestaurants,
+  allCrimes: state.allCrimes,
+  allTransit: state.allTransit.bartStops.concat(state.allTransit.sfmtaStops)
 });
 
 const mapDispatchToProps = dispatch =>({
   requestAllRestaurants: () => dispatch(requestAllRestaurants()),
-  requestAllCrimes: () => dispatch(requestAllCrimes())
+  requestAllCrimes: () => dispatch(requestAllCrimes()),
+  requestFilteredCrimes: (lat, lon, radius) => dispatch(requestFilteredCrimes(lat, lon, radius)),
+  requestAllTransit: () => dispatch(requestAllTransitData())
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Map);
