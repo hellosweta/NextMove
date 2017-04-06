de34e6380951762c1f8adba84e074b63874de1cf
Daniel <daniel.j.fletcher2@gmail.com>
2017-04-06 12:37:22 -0700

    Write Filtered transit reducer and add to root reducer

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -4,14 +4,16 @@ import AllRestaurantsReducer from './all_restaurants_reducer';
 import FilteredRestaurantsReducer from './filtered_restaurants_reducer';
 import AllCrimesReducer from './all_crimes_reducer';
 import FilteredCrimesReducer from './filtered_crimes_reducer';
-import TransitReducer from './transit_reducer';
+import AllTransitReducer from './all_transit_reducer';
+import FilteredTransitReducer from './filtered_transit_reducer';
 
 const RootReducer = combineReducers({
   allRestaurants: AllRestaurantsReducer,
   filteredRestaurants: FilteredRestaurantsReducer,
   allCrimes: AllCrimesReducer,
   filteredCrimes: FilteredCrimesReducer,
-  transit: TransitReducer
+  allTransit: AllTransitReducer,
+  filteredTransit: FilteredTransitReducer
 });
 
 export default RootReducer;
