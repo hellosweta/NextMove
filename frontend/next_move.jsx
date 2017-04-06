c71cc642dde2cd6d8cd88b66aa85ea452ed9b2ce
Daniel <daniel.j.fletcher2@gmail.com>
2017-04-05 21:16:48 -0700

    Add gems to Gemfile, remove redundant jquery script tag from head, delete unecessary index.html file

--- frontend/next_move.jsx
+++ frontend/next_move.jsx
@@ -5,6 +5,9 @@ import Root from './components/root';
 import {
   requestBartData, requestSfmtaData, requestAllTransitData
 } from './actions/transit_actions';
+import { requestAllCrimes } from './actions/crimes_actions';
+
+window.requestAllCrimes = requestAllCrimes;
 
 document.addEventListener('DOMContentLoaded', () => {
   const store = configureStore();
