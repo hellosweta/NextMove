77a04171066df2cbbe8ac0a2a290e88725a44a0a
Sweta <Swetagsanghavi@gmail.com>
2017-04-05 21:14:11 -0700

    Add intensity and favorability scores

--- lib/components/map/map.jsx
+++ lib/components/map/map.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
-// import HeatmapLayer from '../../../assets/vendors/HeatmapLayer';
 import HeatmapLayer from 'react-leaflet-heatmap-layer';
 // import SideBarContainer from '../sidebar/sidebar_container';
 
@@ -11,18 +10,43 @@ class LeafletMap extends React.Component {
     this.state = {
       lat: 37.7749,
       lng: -122.47791,
-      zoom: 13
+      zoom: 13,
+      crimeFavorabilityScore: 0,
+      transitFavorabilityScore: 0,
+      restaurantFavorabilityScore: 0,
+
     };
+    this.getIntensity = this.getIntensity.bind(this);
+    this.getWeights = this.getWeights.bind(this);
   }
 
   componentDidMount(){
     this.props.requestAllRestaurants();
     this.props.requestAllCrimes();
+    this.props.requestAllTransit();
+    this.getWeights();
+  }
+  getWeights(){
+    const ranks = ["crimes","transit", "restaurants"];
+    let intensitycrimes = 0;
+    let intensitytransit = 0;
+    let intensityrestaurants = 0;
+    ranks.forEach((filter, idx) => {
+      if (ranks.length === 3) {
+        const weights = [.5, .3, .2]
+        eval("intensity" + filter + `=${3 * weights[idx]}`);
+      }
+
+    });
+    this.getIntensity(intensitycrimes, intensitytransit, intensityrestaurants)
+  }
+  getIntensity(crimeIntensity, transitIntensity, restaurantIntensity){
+    let minimum = Math.min(this.props.allRestaurants.length, this.props.allCrimes.length, this.props.allTransit.length)
+    this.state.crimeFavorabilityScore = (1/this.props.allCrimes.length) * minimum * crimeIntensity
+    this.state.transitFavorabilityScore = (1/this.props.allTransit.length) * minimum * transitIntensity
+    this.state.restaurantFavorabilityScore = (1/this.props.allRestaurants.length) * minimum * restaurantIntensity
   }
 
-  intensity(){
-
-  }
   render() {
     const position = [this.state.lat, this.state.lng];
   //   const bart = bartStops.map(el => ([el.stop_lat, el.stop_lon, 60]))
@@ -48,38 +72,43 @@ class LeafletMap extends React.Component {
     //   0.4: '#471967', 0.8: '#258E8C', 1: '#E2E32D'
     // };
     const gradient = {
-     0.1: '#89BDE0', 0.2: '#96E3E6', 0.4: '#82CEB6',
-     0.6: '#FAF3A5', 0.8: '#F5D98B', 1.0: '#DE9A96'
-   };
+      0.1: '#89BDE0', 0.2: '#96E3E6', 0.4: '#82CEB6',
+      0.6: '#FAF3A5', 0.8: '#F5D98B', 1.0: '#DE9A96'
+    };
     const inverse_gradient = {
-     1.0: '#89BDE0', 0.8: '#96E3E6', 0.6: '#82CEB6',
-     0.4: '#FAF3A5', 0.2: '#F5D98B', .1: '#DE9A96'
-   };
+      1.0: '#89BDE0', 0.8: '#96E3E6', 0.6: '#82CEB6',
+      0.4: '#FAF3A5', 0.2: '#F5D98B', .1: '#DE9A96'
+    };
     // Removed the below as props into HeatmapLayer
     // fitBoundsOnLoad
     // fitBoundsOnUpdate
     if (!(this.props.allRestaurants instanceof Array) || !(this.props.allCrimes instanceof Array)) {
       return(<div></div>)
     } else {
-      // const transit = this.props.allTransit.map(el => ([el.lat, el.lon, transitIntensity]))
-      // const restaurants = this.props.allRestaurants.map(el => ([el.lat, el.lon, restaurantIntensity]))
-      // const crimes = this.props.allCrimes.map(el => ([el.lat, el.lon, crimeIntensity]))
-      // let positive_factors = restaurants
-      // let negative_factors = crimes
-      const data = [[37.803664, -122.271604, 4], [37.80787, -122.269029, 3]];
+      const transit = this.props.allTransit.map(el => ([el.lat, el.lon, this.state.transitFavorabilityScore]))
+      const restaurants = this.props.allRestaurants.map(el => ([el.lat, el.lon, this.state.restaurantFavorabilityScore]))
+      const crimes = this.props.allCrimes.map(el => ([el.lat, el.lon, this.state.crimeFavorabilityScore]))
+      let positive_factors = restaurants.concat(transit)
+      let negative_factors = crimes
       return (
         <div>
           <Map style={{height: "100vh"}} center={position} zoom={this.state.zoom} bounds={bounds}>
-
             <HeatmapLayer
-              points={data}
+              points={positive_factors}
+              radius={30}
+              gradient={gradient}
+              longitudeExtractor={m => m[1]}
+              latitudeExtractor={m => m[0]}
+              intensityExtractor={m => parseFloat(m[2])}
+             />
+            <HeatmapLayer
+              points={negative_factors}
               radius={20}
               gradient={inverse_gradient}
               longitudeExtractor={m => m[1]}
               latitudeExtractor={m => m[0]}
               intensityExtractor={m => parseFloat(m[2])}
               blur={30} />
-
             <TileLayer
               attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
               url='https://api.mapbox.com/styles/v1/hellosweta/cj12k3v5n004l2rt89a28igfd/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiaGVsbG9zd2V0YSIsImEiOiJjajEyaDhwZnQwNnF5MzNvMms3dzluemZnIn0.RzmThYRkDkV3wEMw7J2JCA'/>
