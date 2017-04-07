a13e2c53c5e35e207be8e88457147d456d82652d
Daniel <daniel.j.fletcher2@gmail.com>
2017-04-06 19:38:38 -0700

    Complete initial styling for header

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -6,7 +6,9 @@ const App = ({children}) =>{
   return(
     <div className='app'>
       <nav className='header'>
-        <h1 className='logo'>NextMoveSF</h1>
+        <h1 className='logo-pt-1'>Next</h1>
+        <h1 className='logo-pt-2'>Move</h1>
+        <h1 className='logo-pt-3'>SF</h1>
       </nav>
       <LeafletMap/>
       { children }
