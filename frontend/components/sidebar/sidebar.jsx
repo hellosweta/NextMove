6f40ca6474dba298fbb2777c4ec04e98e015f0a8
Bruk Argaw <bkargaw@gmail.com>
2017-04-07 04:51:08 -0700

    add a working drag and drop... working on allowing re ordering of data with in one container

--- frontend/components/sidebar/sidebar.jsx
+++ frontend/components/sidebar/sidebar.jsx
@@ -1,24 +1,30 @@
 import React, { Component } from 'react';
 import { DragDropContextProvider } from 'react-dnd';
 import HTML5Backend from 'react-dnd-html5-backend';
-import Dustbin from './dustbin';
+import CategoriesToCalc from './categories_to_calc';
+import CategoriesToChoose from './categories_to_choose';
 import Box from './box';
 
 export default class SideBar extends Component {
-  // constructor(prosp){
-  //   super(props)
-  // }
+  constructor(props){
+    super(props)
+    this.state =({
+      chooseBin: ['Crime', 'Transit', 'Restaurant'],
+      calcBin: []
+    })
+  }
+
   render() {
     return (
       <DragDropContextProvider backend={HTML5Backend}>
         <div>
           <div style={{ overflow: 'hidden', clear: 'both' }}>
-            <Dustbin />
+            <CategoriesToCalc allowedDropEffect="move"
+                     updateRank={this.props.updateRank} />
+            <CategoriesToChoose allowedDropEffect="move"/>
           </div>
           <div style={{ overflow: 'hidden', clear: 'both' }}>
-            <Box name="Crime" />
-            <Box name="Transit" />
-            <Box name="Restaurant" />
+
           </div>
         </div>
       </DragDropContextProvider>
