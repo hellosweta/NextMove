c7795858e9f4a629c618837e49e997022d9440b8
Sweta <Swetagsanghavi@gmail.com>
2017-04-09 21:04:03 -0700

    Add logo, sidebar, and legend stylign

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -6,9 +6,17 @@ const App = ({children}) =>{
   return(
     <div className='app'>
       <nav className='header'>
-        <h1 className='logo-pt-1'>Next</h1>
-        <h1 className='logo-pt-2'>Move</h1>
-        <h1 className='logo-pt-3'>SF</h1>
+        <div>
+          <img className="logo" src="https://image.flaticon.com/icons/svg/188/188467.svg"></img>
+        </div>
+        <div>
+          <div className="full-title">
+
+            <h1 className="title">NextMove    <h1 className="title-SF">SF</h1></h1>
+
+          </div>
+          <h4 className="tagline">Visualize Your Next Home</h4>
+        </div>
       </nav>
       <LeafletMap/>
       { children }
